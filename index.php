a2fd1373c1c173a12e1e7c253984be6cfd4255a2
Lucas Eduardo Romero <lucasromero.cba@hotmail.com>
2019-10-01 11:43:19 -0400

    botao interativo 1/2

--- index.php
+++ index.php
@@ -19,11 +19,24 @@
       Futuro
       </a>
 
+      <span id="nav_item_conta" class="nav-item dropdown mr-auto">
+            <a class="nav-link dropdown-toggle nav-list-item" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="material-icons">account_circle</i>
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Login</a>
+              <a class="dropdown-item" href="#">Logout</a>
+              <a class="dropdown-item" href="#">Perfil</a>
+              <a class="dropdown-item" href="#">Cadastrar</a>
+            </div>
+      </span>
+
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-      </button>
+      </button>      
+
       <div class="collapse navbar-collapse ml-auto" id="navbarNavDropdown">
-        <ul class="navbar-nav mx-auto">
+        <ul class="navbar-nav ml-auto mr-5">
           <li class="nav-item active ml-3 mr-3">
             <a class="nav-link nav-list-item" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
@@ -33,32 +46,39 @@
           <li class="nav-item ml-3 mr-3">
             <a class="nav-link nav-list-item" href="#">Sobre</a>
           </li>
-          <li class="nav-item ml-3 mr-3">
+          <li class="nav-item ml-3 mr-5">
             <a class="nav-link nav-list-item" href="#">Contato</a>
-          </li>
-          <li class="nav-item dropdown ml-3 mr-3">
-            <a class="nav-link dropdown-toggle nav-list-item" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Conta
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">Login</a>
-              <a class="dropdown-item" href="#">Logout</a>
-              <a class="dropdown-item" href="#">Perfil</a>
-              <a class="dropdown-item" href="#">Cadastrar</a>
-            </div>
-            </li>
-          </ul>
+          </li>          
+        </ul>
+      </div>
+
+    </nav>    
+    <div class="section-1">
+      <!--
+      <video id="BG-video" class="BG-media" poster="images/BG-site.jpg" autoplay muted loop>
+        <source src="BG-video.mp4" type="video/mp4">
+      </video>
+      -->
+      <img id="BG-img" class="BG-media" src="images/bg-site.jpg" alt="Engineering Project">
+
+      <div id="btn_logo" class="container">        
+        <div class="row">
+        <div class="col" id="col_box">
+          <div id="box_1" class="box">
+            <div class="logo_text">Projetos</div>
+          </div>
+          <div id="box_2"class="box"> 
+            <div class="logo_text">Cursos</div>
+          </div>
+          <div id="box_3"class="box">
+            <div class="logo_text">Obras</div>
+          </div>
+          </div>
         </div>
-      </nav>
-    <div class="container-fluid section-1">
-
-
+      </div>
+         
     </div>
-    
-    
-      
-      <!-- Optional JavaScript -->
-   
+      <!-- Optional JavaScript -->   
       <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
